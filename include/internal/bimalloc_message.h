2ddc9c0865e04d879b50f10bb145bc6609c4ed63
Nikolay Hohsadze <koliagogsadze@gmail.com>
2021-12-25 14:54:42 +0200

    Add custom assert (#3)
    * Add custom assert
    
    * Remove redundant macro BIMALLOC_MAX_SIZE_OUTPUT

---
+++ include/internal/bimalloc_message.h
@@ -0,0 +1,6 @@
+#ifndef BIMALLOC_BIMALLOC_MESSAGE_INL
+#define BIMALLOC_BIMALLOC_MESSAGE_INL
+
+void bi_show_message_log(const char* fmt, ...);
+
+#endif  // BIMALLOC_BIMALLOC_MESSAGE_INL
---
+++ src/bimalloc_message.c
@@ -0,0 +1,21 @@
+#include "internal/bimalloc_message.h"
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+
+static void bi_vfprintf(const char* fmt, va_list args) {
+  char buffer[512];
+  if (!fmt) {
+    return;
+  }
+  vsnprintf(buffer, sizeof(buffer) - 1, fmt, args);
+  printf("%s", buffer);
+}
+
+void bi_show_message_log(const char* fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  bi_vfprintf(fmt, args);
+  va_end(args);
+}
