e4acce286a0d7ba60be7030ec498d3c19542b8ba
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-12-25 14:53:20 +0200

    Remove redundant macro BIMALLOC_MAX_SIZE_OUTPUT

--- src/bimalloc_message.c
+++ src/bimalloc_message.c
@@ -4,17 +4,13 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#ifndef BIMALLOC_MAX_SIZE_OUTPUT
-#define BIMALLOC_MAX_SIZE_OUTPUT ((size_t)(16 * 1024))
-#endif
-
 static void bi_vfprintf(const char* fmt, va_list args) {
   char buffer[512];
   if (!fmt) {
     return;
   }
   vsnprintf(buffer, sizeof(buffer) - 1, fmt, args);
-  printf("%s",buffer);
+  printf("%s", buffer);
 }
 
 void bi_show_message_log(const char* fmt, ...) {
